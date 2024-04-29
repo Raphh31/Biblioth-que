ae9daa8e965ce88ee73b5e6844720c50f639f86c
Awiisko <leo.dibattitsta31770@gmail.com>
2024-04-29 21:34:12 +0200

    routes

--- appstarter/app/Config/Routes.php
+++ appstarter/app/Config/Routes.php
@@ -6,8 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
-$routes->get('/login', 'Connection::index');
-$routes->post('/login', 'Connection::attemptLogin');
+$routes->get('/login', 'Connection::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
+$routes->post('/login', 'Connection::attemptLogin', ['filter' => \App\Filters\AuthenticatedFilter::class]);
 $routes->get('/home', 'Home::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
 $routes->get('/gestiondeslivres', 'GestionLivres::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
 $routes->get('/gestiondesabonne', 'GestionAbonne::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
