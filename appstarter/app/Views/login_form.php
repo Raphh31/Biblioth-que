f8d18a4e1c7fe8842c0fb1e624cad235b937465f
Raphael SANCHEZ <raphael.sanchez31120@gmail.com>
2024-04-29 12:41:50 +0200

    login_form information

--- appstarter/app/Views/login_form.php
+++ appstarter/app/Views/login_form.php
@@ -19,6 +19,16 @@ admin</label>
         <input id="password" name="password" type="password" />
         <button type="submit">Se connecter</button>
     </form>
+    <H1>LIVRE EN TENDANCE : </H1>
+    <H2>Harry Potter et la Chambre des secrets</H2>
+    <H2>Harry Potter à l'école des sorciers</H2>
+    <H2>Harry Potter et le Prisonnier d'Azkaban</H2>
+    <H2>Harry Potter et la Coupe de feu</H2>
+    <H2>Harry Potter et l'Ordre du phénix</H2>
+    <H2>Harry Potter et le Prince de sang-mêlé</H2>
+    <H1>EVENEMENT ENCHANTEMENT DU 20/04 AU 30/04</H1>
+    <H2>Enchante ton livre preféré avec la table d'enchantement</H2>
+    <h2>CLUTCH I, CLUTCH II, CLUTCH III et plus encore...</h2>
 </body>
 
 </html>
