607b96bea234fb714243c9dba7826491a3f1a65f
Awiisko <leo.dibattitsta31770@gmail.com>
2024-04-29 21:39:08 +0200

    routes correction

--- appstarter/app/Config/Routes.php
+++ appstarter/app/Config/Routes.php
@@ -2,16 +2,11 @@
 
 use CodeIgniter\Router\RouteCollection;
 
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'Home::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
-$routes->get('/login', 'Connection::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
-$routes->post('/login', 'Connection::attemptLogin', ['filter' => \App\Filters\AuthenticatedFilter::class]);
+/**@var RouteCollection $routes*/
+
+$routes->get('/', 'Hos  me::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
+$routes->get('/login', 'Connection::index');
+$routes->post('/login', 'Connection::attemptLogin');
 $routes->get('/home', 'Home::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
 $routes->get('/gestiondeslivres', 'GestionLivres::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
 $routes->get('/gestiondesabonne', 'GestionAbonne::index', ['filter' => \App\Filters\AuthenticatedFilter::class]);
-$routes->get('/supprimeAbo', 'GestionAbonne::SupprimeAbonne', ['filter' => \App\Filters\AuthenticatedFilter::class]);
-$routes->get('/modifyAbo', 'GestionAbonne::ModifyAbonne', ['filter' => \App\Filters\AuthenticatedFilter::class]);
-$routes->post('/modifyAbo', 'GestionAbonne::ModifyAbonne',['filter' => \App\Filters\AuthenticatedFilter::class]);
-
