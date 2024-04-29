70b785ce8147eb04f5c51103cb346b6dac0876a6
Awiisko <leo.dibattitsta31770@gmail.com>
2024-04-29 20:23:59 +0200

    gestion livres/abonne/style

--- appstarter/app/Views/gestiondesabonne.php
+++ appstarter/app/Views/gestiondesabonne.php
@@ -3,29 +3,29 @@
 <body>
 <h1>Gestion des abonné</h1>
 
-    <table>
+    <table id=tababonne>
             <thead>
                 <tr>
-                    <th>matricule_abonne </th>
-                    <th>nom_abonne </th>
-                    <th>date_naissance_abonne </th>
-                    <th>date_adhesion_abonne </th>
-                    <th>adresse_abonne </th>
-                    <th>telephone_abonne </th>
-                    <th>CSP_abonne </th>
+                    <th class=abonne>matricule_abonne </th>
+                    <th class=abonne>nom_abonne </th>
+                    <th class=abonne>date_naissance_abonne </th>
+                    <th class=abonne>date_adhesion_abonne </th>
+                    <th class=abonne>adresse_abonne </th>
+                    <th class=abonne>telephone_abonne </th>
+                    <th class=abonne>CSP_abonne </th>
                 </tr>
             </thead>
             <tbody>
                     <?php foreach ($abonnes as $abonne) : ?>
                 <tr>
-                    <td><?php echo $abonne['matricule_abonne']; ?></td>
-                    <td><?php echo $abonne['nom_abonne']; ?></td>
-                    <td><?php echo $abonne['date_naissance_abonne']; ?></td>
-                    <td><?php echo $abonne['date_adhesion_abonne']; ?></td>
-                    <td><?php echo $abonne['adresse_abonne']; ?></td>
-                    <td><?php echo $abonne['telephone_abonne']; ?></td>
-                    <td><?php echo $abonne['CSP_abonne']; ?></td>
-                    <td><a href="/gestiondesabonne?matricule_abonne=<?= $abonne['matricule_abonne'] ?>">Supprimé</a></td>
+                    <td class=abonne><?php echo $abonne['matricule_abonne']; ?></td>
+                    <td class=abonne><?php echo $abonne['nom_abonne']; ?></td>
+                    <td class=abonne><?php echo $abonne['date_naissance_abonne']; ?></td>
+                    <td class=abonne><?php echo $abonne['date_adhesion_abonne']; ?></td>
+                    <td class=abonne><?php echo $abonne['adresse_abonne']; ?></td>
+                    <td class=abonne><?php echo $abonne['telephone_abonne']; ?></td>
+                    <td class=abonne><?php echo $abonne['CSP_abonne']; ?></td>
+                    <td class=abonne><a href="/gestiondesabonne?matricule_abonne=<?= $abonne['matricule_abonne'] ?>">Supprimé</a></td>
     
                 </tr>
             <?php endforeach; ?>
--- appstarter/app/Views/templates/header.php
+++ appstarter/app/Views/templates/header.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
-<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/styles.css');?>"/>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/styles.css'); ?>" />
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/styles.css');?>"/>
-    <script src="<?php echo base_url('assets/JS/Scripts.js');?>"></script>    
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/styles.css'); ?>" />
+    <script src="<?php echo base_url('assets/JS/Scripts.js'); ?>"></script>
 </head>
 <header>
-<title>BIBLIOTHEQUE G X L</title> 
-<nav>
-        <ul class="sous-menu">
-        <div class="text-gradient">
-            <li><a href="/home">MENU</a></li>
-            <li><a href="/home">Accueil</a></li>
-            <li><a href="/login"> Espace Abonné </a></li>
-            <li><a href="/gestiondeslivres">Gestion des livres</a></li>
-            <li><a href="/gestiondesabonne">Gestion des Abonnées</a></li>
-    </ul>
-</div>    
+    <title>BIBLIOTHEQUE G X L</title>
+    <nav>
+        <table>
+            <tr class="sous-menu">
+                <th><a href="/home">MENU</a></th>
+                <th><a href="/home">Accueil</a></th>
+                <th><a href="/login">Espace Abonné </a></th>
+                <th><a href="/gestiondeslivres">Gestion des livres</a></th>
+                <th><a href="/gestiondesabonne">Gestion des Abonnées</a></th>
+            </tr>
+        </table>
+        </div>
 
-<?php if (isset($loggedIn) && $loggedIn == true) : ?>
-        <span>Bonjour, <?= esc($name) ?></span>
-    <?php endif ?>
-</li></ul></nav>
-
-     
+        <?php if (isset($loggedIn) && $loggedIn == true) : ?>
+            <span>Bonjour, <?= esc($name) ?></span>
+        <?php endif ?>
+        </li>
+        </th>
+    </nav>
