8350a0b4490b33e35782eb3c385cf2a0ca50a016
Awiisko <leo.dibattitsta31770@gmail.com>
2024-04-30 11:30:53 +0200

    FILTER ADMIN

--- appstarter/app/Controllers/GestionLivres.php
+++ appstarter/app/Controllers/GestionLivres.php
@@ -9,10 +9,10 @@ class GestionLivres extends BaseController
     {
 
         $Livre = model(\App\Models\Livre::class);
-        $livres = $Livre->getLivre();
+        $livres = $Livre->getLivres();
         $data['livres']=$livres;
         $session=session();
-
+        
         $template =
             view('templates/Header.php',[
             'loggedIn' => $session->get('loggedIn'),
@@ -24,56 +24,4 @@ class GestionLivres extends BaseController
         return $template;
     }
 
-    public function creeLiv()    
-    {
-        $Livre = model(\App\Models\Livre::class);
-        $livres = $Livre->getLivre();
-        $data['livres']=$livres;
-        $session=session();
-
-        $template =
-            view('templates/Header.php',[
-            'loggedIn' => $session->get('loggedIn'),
-            'name' => $session->get('username')
-            ]).
-            
-            view('creelivre',$data).
-            view('templates/footer');
-        return $template;
-    }
-
-
-    public function ModifyLivre() {
-        $Livre = model(\App\Models\Livre::class);
-        $values = $this->request->getPost(['code_catalogue', 'titre_livre', 'theme_livre']);
-        $Livre->ModifyLivre($values);
-        
-        return redirect()->to('gestiondeslivres');
-    }
-
-    public function AJouteLivre() {
-        $Livre = model(\App\Models\Livre::class);
-        $values = $this->request->getPost(['code_catalogue', 'titre_livre', 'theme_livre']);
-        $Livre->   ADDLivre($values);
-        return redirect()->to('gestiondeslivres');
-    }
-
-
-    public function SuppLivre()
-    {
-        $code = $this->request->getGet();
-        $Livre = model(\App\Models\Livre::class);
-        $Livre->SuppLivre($code);
-        return redirect()->to('gestiondeslivres');
-
-    }
-
-
-
-
-
-
 }
-
-
-
